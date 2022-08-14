889d5778dd652b481529621c4bf7bd8b7bf018d9
Jenry Ollivierre <jenry@jenryollivierre.com>
2022-08-14 12:28:58 -0400

    WIP

--- README.md
+++ README.md
@@ -6,7 +6,143 @@ A laravel package that fetches only the data you need for creating/updating your
 
 ## Installation
 
-Install with composer `composer require jennosgroup/laravel-request-data`.
+Install with composer, via command `composer require jennosgroup/laravel-request-data`.
+
+## Getting Started
+
+We recommend that you create a RequestData folder in your app directory. Then create an abstraction within that directory.
+
+```php
+<?php
+
+namespace App\RequestData;
+
+use JennosGroup\LaravelRequestData\RequestData as JennosGroupRequestData;
+
+abstract class RequestData extends JennosGroupRequestData
+{
+
+}
+```
+
+Then you create the various classes, which extends your abstraction, to represent the domain you are working with.
+
+```php
+<?php
+
+namespace App\RequestData;
+
+class PostRequestData extends RequestData
+{
+
+}
+```
 
 ## Usage
+
+We have two implementation, which is used when creating or updating models.
+
+When creating your model, in a typical store method of your controller, use the `getForCreate` method, which accepts two arguments.
+
+1) An array containing the data to work with i.e `$request->all()`.
+2) The request instance.
+
+When updating your model, in a typical update method of your controller, use the `getForUpdate` method, which accepts three arguments.
+
+1) An array containg the data to work with i.e `$request->all()`.
+2) The model being updated
+3) The request instance.
+
+
+```php
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\RequestData\PostRequestData;
+
+class PostController extends Controller
+{
+	public function store(Request $request)
+	{
+		$data = PostRequestData::getForCreate($request->all(), $request);
+
+		$post = Post::create($data);
+	}
+
+	public function update(Request $request, Post $post)
+	{
+		$data = PostRequestData::getForUpdate($request->all(), $post, $request);
+
+		$post->fill($data)->save();
+	}
+}
+```
+
+## Defining Allowed Attributes
+
+Declare a static array $attributes property on your class and list the attributes that are allowed. This will be the allowed attributes for the create and update request.
+
+If you wish to define attributes only for create and update request, use the `$createAttributes` property or `$updateAttributes` property.
+
+```php
+<?php
+
+namespace App\RequestData;
+
+class PostRequestData extends RequestData
+{
+	/**
+	 * The attributes allowed for create an update. These will always be present.
+	 */
+	protected static array $attributes = ['title', 'slug', 'content', 'author_id'];
+
+	/**
+	 * The attributes allowed when create. These will be merged with the $attributes
+	 */
+	protected static array $createAttributes - ['title', 'slug', 'content', 'author_id'];
+
+	/**
+	 * The attributes allowed when updating. These will be merged with the $attributes.
+	 */
+	protected static array $updateAttributes - ['title', 'slug', 'content'];
+}
+```
+
+## Default Values
+
+If you want default values to be added to attributes that are not present on request, you can add them to the `$defaultAttributes` property. As per convention, you can declare defaults only for create or update request by defining them on the `$createDefaultAttributes` and `$updateDefaultAttributes`.
+
+```php
+<?php
+
+namespace App\RequestData;
+
+class PostRequestData extends RequestData
+{
+	/**
+	 * The default attributes for the create an update request, if no value was submitted for an attribute.
+	 */
+	protected static array $defaultAttributes = [
+		'options' => [],
+	];
+
+	/**
+	 * The default attributes for the create request, if no value was submitted for an attribute.
+	 */
+	protected static array $createDefaultAttributes - [
+		// ...
+	];
+
+	/**
+	 * The attributes allowed when updating. These will be merged with the $attributes.
+	 */
+	protected static array $updateDefaultAttributes - [
+		// ...
+	];
+}
+```
+
+## Computing Data
 
---
+++ src/RequestData.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace JennosGroup\LaravelRequestData;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class RequestData
+{
+    /**
+     * The whitelist of attributes for all requests.
+     */
+    protected static array $attributes = [];
+
+    /**
+     * The whitelist of attributes for when a create request is being handled.
+     */
+    protected static array $createAttributes = [];
+
+    /**
+     * The whitelist of attributes for when an update request is being handled.
+     */
+    protected static array $updateAttributes = [];
+
+    /**
+     * The default attributes for all requests.
+     */
+    protected static array $defaultAttributes = [];
+
+    /**
+     * The default attributes for when a create request is being handled.
+     */
+    protected static array $createDefaultAttributes = [];
+
+    /**
+     * The default attributes for when an update request is being handled.
+     */
+    protected static array $updateDefaultAttributes = [];
+
+    /**
+     * Flag to indicate if we are on a create request.
+     */
+    private static bool $isCreating = false;
+
+    /**
+     * Flag to indicate if we are on an update request.
+     */
+    private static bool $isUpdating = false;
+
+    /**
+     * The model to work with for the request.
+     *
+     * This will usually be for the update request as a model will not be
+     * established as yet when creating.
+     */
+    private static ?Model $model = null;
+
+    /**
+     * Get the attributes when handling a create request.
+     *
+     * @param  array  $data
+     * @param  Illuminate\Http\Request  $request
+     *
+     * @return array
+     */
+    public static function getForCreate(array $data, Request $request): array
+    {
+        static::$isCreating = true;
+
+        // Get the data that is allowed
+        $allowedData = array_intersect_key($data, array_flip(static::getCreateAttributes()));
+
+        // Merge any defaults with the allowed data
+        $allowedData = array_merge(static::getCreateDefaultAttributes(), $allowedData);
+
+        // Allows us to further manipulate the data.
+        if (method_exists(static::class, 'compute') && ! method_exists(static::class, 'computeForCreate')) {
+            $allowedData = static::compute($data, $allowedData, $request);
+        }
+
+        // Allows us to further manipulate the data for create request. As you can see,
+        // this takes precedence over the `compute` method.
+        if (method_exists(static::class, 'computeForCreate')) {
+            $allowedData = static::computeForCreate($data, $allowedData, $request);
+        }
+
+        return $allowedData;
+    }
+
+    /**
+     * Get the attributes when handling an update request.
+     *
+     * @param  array  $data
+     * @param  Illuminate\Database\Eloquent\Model  $model
+     * @param  Illuminate\Http\Request  $request
+     *
+     * @return array
+     */
+    public static function getForUpdate(array $data, Model $model, Request $request): array
+    {
+        static::$isUpdating = true;
+        static::$model = $model;
+
+        // Get the data that is allowed
+        $allowedData = array_intersect_key($data, array_flip(static::getUpdateAttributes()));
+
+        // Merge any defaults with the allowed data
+        $allowedData = array_merge(static::getUpdateDefaultAttributes(), $allowedData);
+
+        // Allows us to further manipulate the data.
+        if (method_exists(static::class, 'compute') && ! method_exists(static::class, 'computeForUpdate')) {
+            $allowedData = static::compute($data, $allowedData, $request);
+        }
+
+        // Allows us to further manipulate the data for update request. As you can see,
+        // this takes precedence over the `compute` method.
+        if (method_exists(static::class, 'computeForUpdate')) {
+            $allowedData = static::computeForUpdate($data, $allowedData, $request);
+        }
+
+        return $allowedData;
+    }
+
+    /**
+     * Get the whitelisted attributes for the create request.
+     *
+     * @return array
+     */
+    public static function getCreateAttributes(): array
+    {
+        return array_merge(static::$attributes, static::$createAttributes);
+    }
+
+    /**
+     * Get the whitelisted attributes for the update request.
+     *
+     * @return array
+     */
+    public static function getUpdateAttributes(): array
+    {
+        return array_merge(static::$attributes, static::$updateAttributes);
+    }
+
+    /**
+     * Get the default attributes for the create request.
+     *
+     * @return array
+     */
+    public static function getCreateDefaultAttributes(): array
+    {
+        return array_merge(static::$defaultAttributes, static::$createDefaultAttributes);
+    }
+
+    /**
+     * Get the default attributes for the update request.
+     *
+     * @return array
+     */
+    public static function getUpdateDefaultAttributes(): array
+    {
+        return array_merge(static::$defaultAttributes, static::$updateDefaultAttributes);
+    }
+
+    /**
+     * Check if we are on a create request.
+     *
+     * @return bool
+     */
+    public static function isCreating(): bool
+    {
+        return static::$isCreating;
+    }
+
+    /**
+     * Check if we are on an update request.
+     *
+     * @return bool
+     */
+    public static function isUpdating(): bool
+    {
+        return static::$isUpdating;
+    }
+
+    /**
+     * Get the model.
+     *
+     * @return Illuminate\Database\Eloquent\Model
+     */
+    public static function getModel(): ?Model
+    {
+        return static::$model;
+    }
+}
